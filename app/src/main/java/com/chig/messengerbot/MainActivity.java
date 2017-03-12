ffab5ec23f1af387e9581d28bf99f6344ae9556c
Chirag <chiragv00@gmail.com>
2017-03-12 17:29:22 -0400

    Version 2: DONE

--- app/src/main/java/com/chig/messengerbot/MainActivity.java
+++ app/src/main/java/com/chig/messengerbot/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Handler handler;
     int state = 0;
     int c = 0;
+    String name = "";
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -67,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                         public void run() {
                             //manager.sendTextMessage(messages[0].getOriginatingAddress(), null, "Test", null, null);
                             String message = "";
+                            String message2 = "";
 
                             switch(state){
                                 case 0:
@@ -74,14 +76,21 @@ public class MainActivity extends AppCompatActivity {
                                     textView.setText("State 1: Greeting");
                                     if(c == 0) {
                                         if (messages[0].getMessageBody().toUpperCase().contains("HELLO") || messages[0].getMessageBody().toUpperCase().contains("HI") || messages[0].getMessageBody().toUpperCase().contains("HEY")) {
-                                            message = "Hello and welcome to the wonderful world of Pokémon. My name is Professor Oak, but everyone calls me the Pokémon Professor." +
-                                                   " What is your name?";
+                                            int rand = (int)(Math.random()*2);
+                                            if(rand == 0) {
+                                                message = "Hello and welcome to the wonderful world of Pokémon. My name is Professor Oak, but everyone calls me the Pokémon Professor." +
+                                                        " What is your name?";
+                                            }
+                                            else if(rand == 1){
+                                                message = "Hello how are you doing? My name is Professor Oak, but everyone calls me the Pokémon Professor." +
+                                                        "What is your name?";
+                                            }
                                             c++;
                                         }
                                     }
                                     else if(c == 1){
                                             message = "Hello " + messages[0].getMessageBody() + "! Would you like to learn about the gyms or the pokemon?";
-
+                                            name = messages[0].getMessageBody();
                                             c = 0;
                                             state++;
                                     }
@@ -95,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
                                         state++;
                                     }
                                     else if(messages[0].getMessageBody().toUpperCase().contains("POKEMON")){
-                                        message = "There are many different types of pokemon that you can discover when you explore. " +
+                                        message = "There are many different types of pokemon that you can discover when you explore." +
                                                 " Now, which starter would you pick: Squirtle, Charmander, or Bulbasaur? ";
                                         state++;
                                     }
@@ -103,20 +112,35 @@ public class MainActivity extends AppCompatActivity {
                                     break;
                                 case 2:
                                     textView.setText("State 3: Starter Info");
-                                    if(messages[0].getMessageBody().toUpperCase().contains("SQUIRTLE")){
+                                    if (messages[0].getMessageBody().toUpperCase().contains("SQUIRTLE")) {
                                         message = "Squirtle the turtle is the water starter. Squirtle is a great choice against fire and will eventually evolve into Blastoise.";
                                         state++;
-                                    }
-                                    else if(messages[0].getMessageBody().toUpperCase().contains("CHARMANDER")){
+                                    } else if (messages[0].getMessageBody().toUpperCase().contains("CHARMANDER")) {
                                         message = "Charmander is the fire starter. Charmander is a great choice against grass and will eventually evolve into Charzard";
                                         state++;
-                                    }
-                                    else if(messages[0].getMessageBody().toUpperCase().contains("BULBASAUR")){
+                                    } else if (messages[0].getMessageBody().toUpperCase().contains("BULBASAUR")) {
                                         message = "Balbasaur is the grass starter. Bulbasaur is a great choice against water and will eventually evolve into Venasaur.";
                                         state++;
                                     }
+                                    if(state == 3){
+                                        message2 = "Would you like to learn about the pokedex next, or battle items?";
+                                    }
+
                                     break;
                                 case 3:
+                                    textView.setText("State 4: Exit State");
+                                    if(messages[0].getMessageBody().toUpperCase().contains("POKEDEX")){
+                                        message = "The pokedex is a tool that all trainers should have. It records all the pokemon you have seen and what you have caught";
+                                        state++;
+                                    }
+                                    else if(messages[0].getMessageBody().toUpperCase().contains("BATTLE ITEMS")){
+                                        message = "There are many items that you can use during a battle. Things like potions will help your pokemon a lot during a battle.";
+                                        state++;
+                                    }
+
+                                    if(state == 4){
+                                        message2 = "Well, " + name + " that's all the time I have to talk. It was fun to talk with you. Until next time.";
+                                    }
 
                                     break;
                             }
@@ -125,10 +149,13 @@ public class MainActivity extends AppCompatActivity {
                             if(!message.equals("")){
                                 manager.sendTextMessage(messages[0].getOriginatingAddress(), null, message, null, null);
                             }
+                            if(!message2.equals("")){
+                                manager.sendTextMessage(messages[0].getOriginatingAddress(), null, message2, null, null);
+                            }
 
 
                         }
-                    }, 1000);
+                    }, 10000);
 
             }
         };
